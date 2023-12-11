efb2ab09ecf509c6a0bb808d401665606fdc43e0
Jorge Caballero <jorge.caballero.31.10.98@gmail.com>
2023-12-11 15:06:06 -0300

    Framer motion pruebas

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,29 @@
+"use client";
 import Header from "@/components/Header";
+import { motion } from "framer-motion";
 
 export default function Home() {
+  const animationVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+  };
+
   return (
     <>
       <Header />
       <h1 className="w-1/2 p-2 mx-auto text-2xl italic text-center border">
         Next file sharing
       </h1>
-    </>
+      <motion.div animate={{ x: 250 }} initial={{ x: -250 }} exit={{ x: -250 }}>
+        ¡Hola, mundo!
+      </motion.div>
+      <motion.div
+        variants={animationVariants}
+        initial="hidden"
+        animate="visible"
+      >
+        ¡Hola, mundo!
+      </motion.div>
+      </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,14 @@
 const Header = () => {
   return (
     <>
-      <div className="text-6xl font-semibold text-center text-orange-700 uppercase">Header</div>
+      <div className="flex items-center justify-between p-4 bg-gray-800 text-white">
+        <div className="text-2xl font-semibold">Tu Aplicación</div>
+        <div className="flex space-x-4">
+          <button className="px-4 py-2 bg-blue-500 text-white rounded-md">
+            Agregar Archivos
+          </button>
+        </div>
+      </div>
     </>
   );
 };
