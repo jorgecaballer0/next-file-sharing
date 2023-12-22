9a113ba765267401d6293eb9ae7968c9c2a51c3a
Jorge Caballero <jorge.caballero.31.10.98@gmail.com>
2023-12-22 11:29:24 -0300

    Inicio terminado

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,34 +1,32 @@
+import Constantes from "@/utils/Constantes";
 import React from "react";
 
 const Hero = () => {
   return (
     <>
       <section className="bg-gray-50">
-        <div className="max-w-screen-xl px-4 py-32 mx-auto lg:flex lg:h-screen lg:items-center">
+        <div className="max-w-screen-xl px-4 py-32 mx-auto lg:flex">
           <div className="max-w-xl mx-auto text-center">
             <h1 className="text-3xl font-extrabold sm:text-5xl">
-              Understand User Flow.
-              <strong className="font-extrabold text-red-700 sm:block">
-                {" "}
-                Increase Conversion.{" "}
-              </strong>
+              <span className="text-primary">Upload, Save</span> and easily{" "}
+              <span className="text-primary">Share</span> your files in one
+              place
             </h1>
 
-            <p className="mt-4 sm:text-xl/relaxed">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt
-              illo tenetur fuga ducimus numquam ea!
+            <p className="mt-4 text-gray-500 sm:text-xl/relaxed">
+              {Constantes.desc}
             </p>
 
             <div className="flex flex-wrap justify-center gap-4 mt-8">
               <a
-                className="block w-full px-12 py-3 text-sm font-medium text-white bg-red-600 rounded shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
+                className="block w-full px-12 py-3 text-sm font-medium text-white rounded shadow bg-primary hover:bg-blue-700 focus:outline-none focus:ring active:bg-blue-500 sm:w-auto"
                 href="/get-started"
               >
                 Get Started
               </a>
 
               <a
-                className="block w-full px-12 py-3 text-sm font-medium text-red-600 rounded shadow hover:text-red-700 focus:outline-none focus:ring active:text-red-500 sm:w-auto"
+                className="block w-full px-12 py-3 text-sm font-medium rounded shadow text-primary hover:text-blue-700 focus:outline-none focus:ring active:text-blue-500 sm:w-auto"
                 href="/about"
               >
                 Learn More
---
+++ src/utils/Constantes.js
@@ -0,0 +1,4 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  desc: "Drag and drop your file directly on our cloud and share it with your friends secuarely with password and send it on email",
+};
