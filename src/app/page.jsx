75c76c67e0910f89016973c749aa2d414b9d7a76
Jorge Caballero <jorge.caballero.31.10.98@gmail.com>
2023-12-22 10:58:28 -0300

    Header terminado - Inicio

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 import { motion } from "framer-motion";
 
 export default function Home() {
@@ -11,19 +12,7 @@ export default function Home() {
   return (
     <>
       <Header />
-      <h1 className="w-1/2 p-2 mx-auto text-2xl italic text-center border">
-        Next file sharing
-      </h1>
-      <motion.div animate={{ x: 250 }} initial={{ x: -250 }} exit={{ x: -250 }}>
-        ¡Hola, mundo!
-      </motion.div>
-      <motion.div
-        variants={animationVariants}
-        initial="hidden"
-        animate="visible"
-      >
-        ¡Hola, mundo!
-      </motion.div>
-      </>
+      <Hero />
+    </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,98 @@
+import Image from "next/image";
+
 const Header = () => {
   return (
     <>
-      <div className="flex items-center justify-between p-4 bg-gray-800 text-white">
-        <div className="text-2xl font-semibold">Tu Aplicación</div>
-        <div className="flex space-x-4">
-          <button className="px-4 py-2 bg-blue-500 text-white rounded-md">
-            Agregar Archivos
-          </button>
+      <header className="bg-white">
+        <div className="max-w-screen-xl px-4 mx-auto border-b sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between h-16">
+            <div className="md:flex md:items-center md:gap-12">
+              <Image src="/logo.svg" width={150} height={100} alt="Logo" />
+            </div>
+
+            <div className="hidden md:block">
+              <nav aria-label="Global">
+                <ul className="flex items-center gap-6 text-sm">
+                  <li>
+                    <a
+                      className="text-gray-500 transition hover:text-gray-500/75"
+                      href="/"
+                    >
+                      Home
+                    </a>
+                  </li>
+
+                  <li>
+                    <a
+                      className="text-gray-500 transition hover:text-gray-500/75"
+                      href="/"
+                    >
+                      Upload
+                    </a>
+                  </li>
+
+                  <li>
+                    <a
+                      className="text-gray-500 transition hover:text-gray-500/75"
+                      href="/"
+                    >
+                      About Us
+                    </a>
+                  </li>
+
+                  <li>
+                    <a
+                      className="text-gray-500 transition hover:text-gray-500/75"
+                      href="/"
+                    >
+                      Contact Us
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+
+            <div className="flex items-center gap-4">
+              <div className="sm:flex sm:gap-4">
+                <a
+                  className="rounded-md bg-primary px-5 py-2.5 text-sm font-medium text-white shadow transition-colors hover:bg-blue-700"
+                  href="/"
+                >
+                  Get Started
+                </a>
+
+                <div className="hidden sm:flex">
+                  <a
+                    className="rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600"
+                    href="/"
+                  >
+                    Register
+                  </a>
+                </div>
+              </div>
+
+              <div className="block md:hidden">
+                <button className="p-2 text-gray-600 transition bg-gray-100 rounded hover:text-gray-600/75">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-5 h-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
+      </header>
     </>
   );
 };
